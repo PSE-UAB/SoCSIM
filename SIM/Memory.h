ad112f2aa590325e89d36c8b1fdb1c20e0df9c13
Marius <marius.monton@gmail.com>
2021-04-14 01:07:01 +0200

    Logic operators, code clean-up

--- SIM/Memory.h
+++ SIM/Memory.h
@@ -15,39 +15,43 @@
 #include <utility>
 
 enum {
-    ADDR_PORTA_CTRL = 0x1000,
-    ADDR_PORTA_INT = 0x1004,
-    ADDR_PORTA_OUT = 0x1008,
-    ADDR_PORTA_IN = 0x100C,
-    ADDR_PORTB_CTRL = 0x2000,
-    ADDR_PORTB_INT = 0x2004,
-    ADDR_PORTB_OUT = 0x2008,
-    ADDR_PORTB_IN = 0x200C,
-    ADDR_PORTC_CTRL = 0x3000,
-    ADDR_PORTC_INT = 0x3004,
-    ADDR_PORTC_OUT = 0x3008,
-    ADDR_PORTC_IN = 0x300C,
-    ADDR_PORTD_CTRL = 0x4000,
-    ADDR_PORTD_INT = 0x4004,
-    ADDR_PORTD_OUT = 0x4008,
-    ADDR_PORTD_IN = 0x400C,
-    ADDR_NVIC_CTRL = 0xA000,
-    ADDR_NVIC_IRQ = 0xA004,
-    ADDR_I2C0_CTRL = 0xB000,
-    ADDR_TIMER_CTRL = 0xC000,
-    ADDR_TIMER_TOP = 0xC004,
-    ADDR_TIMER_CNT = 0xC008,
-    ADDR_TIMER_CMP = 0xC00C,
-    ADDR_RTC_CTRL = 0xD000,
-    ADDR_RTC_CNT = 0xD004,
-    ADDR_RTC_CMP = 0xD008,
-    ADDR_TRACE = 0xF000,
-    ADDR_DAC_CTRL = 0x10000,
-    ADDR_DAC_DATA = 0x10004,
-    ADDR_UART_CTRL = 0x20000,
+    ADDR_PORTA_CTRL  = 0x01000,
+    ADDR_PORTA_INT   = 0x01004,
+    ADDR_PORTA_OUT   = 0x01008,
+    ADDR_PORTA_IN    = 0x0100C,
+    ADDR_PORTB_CTRL  = 0x02000,
+    ADDR_PORTB_INT   = 0x02004,
+    ADDR_PORTB_OUT   = 0x02008,
+    ADDR_PORTB_IN    = 0x0200C,
+    ADDR_PORTC_CTRL  = 0x03000,
+    ADDR_PORTC_INT   = 0x03004,
+    ADDR_PORTC_OUT   = 0x03008,
+    ADDR_PORTC_IN    = 0x0300C,
+    ADDR_PORTD_CTRL  = 0x04000,
+    ADDR_PORTD_INT   = 0x04004,
+    ADDR_PORTD_OUT   = 0x04008,
+    ADDR_PORTD_IN    = 0x0400C,
+    ADDR_NVIC_CTRL   = 0x0A000,
+    ADDR_NVIC_IRQ    = 0x0A004,
+    ADDR_I2C0_CTRL   = 0x0B000,
+    ADDR_TIMER_CTRL  = 0x0C000,
+    ADDR_TIMER_TOP   = 0x0C004,
+    ADDR_TIMER_CNT   = 0x0C008,
+    ADDR_TIMER_CMP   = 0x0C00C,
+    ADDR_RTC_CTRL    = 0x0D000,
+    ADDR_RTC_CNT     = 0x0D004,
+    ADDR_RTC_CMP     = 0x0D008,
+    ADDR_TRACE       = 0x0F000,
+    ADDR_DAC_CTRL    = 0x10000,
+    ADDR_DAC_DATA    = 0x10004,
+    ADDR_UART_CTRL   = 0x20000,
     ADDR_UART_STATUS = 0x20004,
     ADDR_UART_TXDATA = 0x20008,
     ADDR_UART_RXDATA = 0x2000C,
+    ADDR_ADC_ADMUX   = 0x30000,
+    ADDR_ADC_CTRL    = 0x30004,
+    ADDR_ADC_DATA    = 0x30008,
+    ADDR_ADC_STATUS  = 0x3000C,
 };
 
 /**
@@ -78,7 +82,7 @@ struct WordMem {
         return data;
     }
 
-    int operator &=(int p_data) {
+    uint32_t operator &=(int p_data) {
         if (cb_rd) {
             cb_rd(data, param_wr);
         }
@@ -92,13 +96,31 @@ struct WordMem {
         return data;
     }
 
-    int operator |=(int p_data) {
+    uint32_t operator |=(int p_data) {
+        if (cb_rd) {
+            cb_rd(data, param_wr);
+        }
+
         data = data | p_data;
+
+        if (cb_wr) {
+            cb_wr(data, param_wr);
+        }
+
         return data;
     }
 
-    int operator ^=(int p_data) {
+    uint32_t operator ^=(int p_data) {
+
+        if (cb_rd) {
+            cb_rd(data, param_wr);
+        }
+
         data = data ^ p_data;
+
+        if (cb_wr) {
+            cb_wr(data, param_wr);
+        }
         return data;
     }
 
